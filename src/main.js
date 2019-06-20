2f9e2b4c4480b72fe7cee1fc28dfec38c2222273
Lee, Jeongmyeong <jeongmyeong.lee@rakuten.com>
2019-06-20 17:49:07 +0900

    ES6

--- src/main.js
+++ src/main.js
@@ -4,27 +4,24 @@ function encrypt(original, app_key) {
     if (app_key.startsWith(BASE_64_PREFIX)) {
         app_key = app_key.replace(BASE_64_PREFIX, '');
     }
-    var key = CryptoJS.enc.Base64.parse(app_key);
-    var iv = CryptoJS.lib.WordArray.random(16);
+    const key = CryptoJS.enc.Base64.parse(app_key);
+    const iv_byte = CryptoJS.lib.WordArray.random(16);
 
-    var value = CryptoJS.AES.encrypt(original, key, {iv: iv}).toString();
-    var iv = CryptoJS.enc.Base64.stringify(iv);
-    var mac = CryptoJS.HmacSHA256(iv+value, key).toString();
+    const value = CryptoJS.AES.encrypt(original, key, {iv: iv_byte}).toString();
+    const iv = CryptoJS.enc.Base64.stringify(iv_byte);
+    const mac = CryptoJS.HmacSHA256(iv+value, key).toString();
 
-    var json = {"iv":iv, "value":value, "mac":mac};
-
-    var encodedJson = CryptoJS.enc.Utf8.parse(JSON.stringify(json).toString());
-    return CryptoJS.enc.Base64.stringify(encodedJson);
+    const json = JSON.stringify({"iv":iv, "value":value, "mac":mac}).toString();
+    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(json));
 }
 
 function decrypt(payload, app_key) {
     if (app_key.startsWith(BASE_64_PREFIX)) {
         app_key = app_key.replace(BASE_64_PREFIX, '');
     }
-    var decoded = CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(payload));
-    var json = JSON.parse(decoded);
-    var key = CryptoJS.enc.Base64.parse(app_key);
-    var iv = CryptoJS.enc.Base64.parse(json.iv);
+    const json = JSON.parse(CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(payload)));
+    const key = CryptoJS.enc.Base64.parse(app_key);
+    const iv = CryptoJS.enc.Base64.parse(json.iv);
 
     return CryptoJS.AES.decrypt(json.value, key, {iv: iv}).toString(CryptoJS.enc.Utf8);
 }
